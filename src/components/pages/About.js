b22d60778175b61b6e49d91dac90772c3c744b39
tanmaysihare <tanmaysihare@gmail.com>
2023-07-20 01:43:53 +0530

    fetch Movies tasks 9

---
+++ src/components/pages/About.js
@@ -0,0 +1,4 @@
+function AboutPage(){
+    return <h1>My About page</h1>;
+}
+export default AboutPage;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,74 @@
+import React, { useState,useEffect,useCallback } from "react";
+
+import MoviesList from "../Movies/MoviesList";
+import "./Home.css";
+
+function HomePage(){
+    const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  let retry = true;
+  
+
+  const  fetchMoviesHandler = useCallback(async() => {
+    setIsLoading(true);
+    setError(null);
+    let retry = true;
+    while (retry) {
+    try {
+        const response = await fetch("http://swapi.dev/api/films/")
+        if (!response.ok){
+            throw new Error('something went wrong!');
+           }
+        const data = await response.json();
+  
+       
+         const transformedMovies = data.results.map(moviesData => {
+           return {
+             id: moviesData.episode_id,
+             title:moviesData.title,
+             openingText:moviesData.opening_crawl,
+             releaseDate: moviesData.release_date
+           };
+         });
+         setMovies(transformedMovies);
+       retry = false;
+    } catch (error){
+        setError("something went wrong...Retrying");
+        await new Promise((resolve) => setTimeout(resolve,5000));
+        setIsLoading(false);
+    }
+}
+    setIsLoading(false);
+},[]);
+useEffect(()=>{
+    fetchMoviesHandler();
+ },[fetchMoviesHandler]);
+  
+  function cancelHandler () {
+    retry = false;
+    setError("Stop Retrying");
+  }
+
+let content = <p>Found no movies.</p>;
+if(movies.length > 0){
+    content = <MoviesList movies={movies} />;
+}
+if(error) {
+    content = <p>{error}<button onClick={cancelHandler}>Cancel Retrying</button></p>;
+}
+if(isLoading){
+    content = <p>Loading...</p>;
+}
+    return <>
+           <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+      {content}
+      </section>  
+        
+    </>;
+}
+export default HomePage;
